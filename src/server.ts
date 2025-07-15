c34764877e33a9ba387c44b8026ddb9ff619976f
Zandrelius <zandreliusakain1@gmail.com>
2025-07-15 16:06:22 +0300

    changes

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,6 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: "*",
-
     //     "http://localhost:5173",
     //     "https://blog-it-fe-42k8.vercel.app"
     // ],
